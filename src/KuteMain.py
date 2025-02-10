501ebb6140d36979379599a2fc4e5349613084db
jiamin <jiamin@moonshot.cn>
2025-02-10 13:05:50 +0800

    增加两个comfyUI的执行流程

--- src/KuteMain.py
+++ src/KuteMain.py
@@ -1,15 +1,18 @@
+import shutil
 import uuid
 
 import os
 
 from PIL import Image, ImageEnhance
 
+from src.utils.ComfyUIHandle import ComfyUIHandle
 from src.utils.ImageHandle import ImageHandle
 
 class KuteChangeFabricHandler:
     def __init__(self):
         self.resize_folder =  r"C:\Users\pc\Desktop\kute2025\resize"
         self.layer_preprocess_folder = r"C:\Users\pc\Desktop\kute2025\layer_preprocess"
+        # layer_preprocess_folder存在二级目录，请在这里管理，目录一定要和comfyUI json的路径一致
         self.resize_repeat_folder = r"C:\Users\pc\Desktop\kute2025\resize_repeat"
         self.mask_folder = r"C:\Users\pc\Desktop\kute2025\mask"
         self.bright_folder = r"C:\Users\pc\Desktop\kute2025\bright"
@@ -17,6 +20,9 @@ class KuteChangeFabricHandler:
         self.multiply_folder = r"C:\Users\pc\Desktop\kute2025\multiply"
         self.rembg_folder = r"C:\Users\pc\Desktop\kute2025\rembg"
         self.sdxl_folder = r"C:\Users\pc\Desktop\kute2025\sdxl"
+
+
+        self.comfyHandle = ComfyUIHandle()
 
     def change_fabric(self, cloth_img_path, fabric_img_path):
         # 这个图像的唯一ID
@@ -31,12 +37,26 @@ class KuteChangeFabricHandler:
         resize_image.save(resize_save_path, "PNG")
 
         # 第二步骤 调用一个comfyUI layer preprocess
-        # todo 要根据prompt json进行适配（参数太多，还没搞）
-        layer_preprocess_image = "徐京你先写死一个你生成好的本地地址"
+        layer_preprocess_response =  self.comfyHandle.generate_image(resize_save_path)
+        code = layer_preprocess_response.get('code')
+        # 根据返回的 code 进行不同的处理
+        layer_preprocess_image_name = ""
+        if code == 0:
+            # 图片生成成功，返回路径
+            data = layer_preprocess_response.get('data')
+            if data:
+                layer_preprocess_image_name = data
+            else:
+                return {"status": "error", "message": "layer_preprocess没有返回图片路径"}
+        else:
+            return {"status": "error", "message": "未知错误，layer_preprocess 返回了未知的 code"}
+        layer_preprocess_image_final_name = os.path.join(self.layer_preprocess_folder, f"{unique_id}.png")
+        shutil.move(layer_preprocess_image_name, layer_preprocess_image_final_name)
 
         # 第三步骤
         # 3.1.1 resize_repeat
-        resize_repeat_image = ImageHandle.tile_image(resize_save_path)
+        #todo  这个步骤中要输入的是layer_preprocess二级文件夹下的哪一个文件？需要修改
+        resize_repeat_image = ImageHandle.tile_image(layer_preprocess_image_final_name)
         # 将resize_repeat后的图片存储到一个固定的文件夹里
         resize_repeat_save_path = os.path.join(self.resize_repeat_folder, f"{unique_id}.png")
         resize_repeat_image.save(resize_repeat_save_path, "PNG")
@@ -61,14 +81,13 @@ class KuteChangeFabricHandler:
         ImageHandle.process_rembg(multiply_save_path, mask_save_path, rembg_save_path)
 
         # 3.3 layer_merge
-        layer_merge_image = ImageHandle.layer_merge(layer_preprocess_image, multiply_save_path, fabric_img_path)
+        layer_merge_image = ImageHandle.layer_merge(layer_preprocess_image_final_name, multiply_save_path, fabric_img_path)
         layer_merge_save_path = os.path.join(self.layer_merge_folder, f"{unique_id}.png")
         layer_merge_image.save(layer_merge_save_path, "PNG")
 
         # 第四步骤
         # 4.1 comfyUI sdxl
         # todo 要根据json进行适配
-
 
         # Final 返回处理完成后的图片在本地的地址(先返回第三步骤最后的文件)
         return layer_merge_save_path
